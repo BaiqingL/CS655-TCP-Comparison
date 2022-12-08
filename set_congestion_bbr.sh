e8c878819cbeca4b3975d9f7f0707b0fc5f067bb
BaiqingL <me@baiqinglyu.com>
2022-12-07 21:23:07 -0500

    [Feat] Mass set congestion on all machines

---
+++ set_congestion_bbr.sh
@@ -0,0 +1,7 @@
+ssh baiqing@pc5.genirack.nyu.edu -p 30610 sudo sysctl -w net.ipv4.tcp_congestion_control=bbr
+ssh baiqing@pc5.genirack.nyu.edu -p 30611 sudo sysctl -w net.ipv4.tcp_congestion_control=bbr
+ssh baiqing@pc5.genirack.nyu.edu -p 30612 sudo sysctl -w net.ipv4.tcp_congestion_control=bbr
+ssh baiqing@pc5.genirack.nyu.edu -p 30613 sudo sysctl -w net.ipv4.tcp_congestion_control=bbr
+ssh baiqing@pc5.genirack.nyu.edu -p 30614 sudo sysctl -w net.ipv4.tcp_congestion_control=bbr
+ssh baiqing@pc2.genirack.nyu.edu -p 30610 sudo sysctl -w net.ipv4.tcp_congestion_control=bbr
+ssh baiqing@pc1.genirack.nyu.edu -p 30610 sudo sysctl -w net.ipv4.tcp_congestion_control=bbr
